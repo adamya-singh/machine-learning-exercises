26cb39dee3f14d4e373e183e9eb146ac2f74066f
adamya-singh <7adamyasingh@gmail.com>
2024-12-16 14:40:18 -0500

    ChromaDB requires 'tokenizers<=0.20.3' while transformers (needed to run stella 1.5B embedding model) requires 'tokenizers>=0.21', so temporarily replacing ChromaDB with Pinecone

--- first-RAG-langchain-project/create_database.py
+++ first-RAG-langchain-project/create_database.py
@@ -3,23 +3,29 @@ from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.schema import Document
 from langchain_openai import OpenAIEmbeddings
 from langchain_community.vectorstores import Chroma
+from langchain_pinecone import Pinecone
+import pinecone
 from dotenv import load_dotenv
 import os
 import shutil
 
 CHROMA_PATH = "chroma"
 DATA_PATH = "data/current_data"
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+PINECONE_API_KEY = os.getenv("PINECONE_API_KEY")
 
 def main():
     #only runs if the file is run directly, not if called as a module
     load_dotenv()
     openai_api_key = os.getenv("OPENAI_API_KEY")
-    generate_data_store(openai_api_key)
+    pinecone_api_key = os.getenv("PINECONE_API_KEY")
+    generate_data_store(openai_api_key, pinecone_api_key)
 
-def generate_data_store(openai_api_key):
+def generate_data_store(openai_api_key, pinecone_api_key):
     documents = load_documents()
     chunks = split_text(documents)
-    save_to_chroma(chunks, openai_api_key)
+    #save_to_chroma(chunks, openai_api_key)
+    save_to_pinecone(chunks, openai_api_key, pinecone_api_key)
 
 def load_documents():
     loader = DirectoryLoader(DATA_PATH, glob="*.md")
@@ -43,7 +49,7 @@ def split_text(documents: list[Document]):
     print(document.metadata)
 
     return chunks
-
+"""
 def save_to_chroma(chunks: list[Document], openai_api_key):
     #remove existing database if present
     if os.path.exists(CHROMA_PATH):
@@ -57,6 +63,29 @@ def save_to_chroma(chunks: list[Document], openai_api_key):
     )
     db.persist()
     print(f"Saved {len(chunks)} chunks to {CHROMA_PATH}.")
+"""
+
+def save_to_pinecone(chunks: list[Document], openai_api_key, pinecone_api_key):
+    pinecone.init(api_key=pinecone_api_key, environment="us-east-1")
+    
+    #name of the vector database on pinecone website
+    index_name = "rutgers-menu-assistant"
+
+    if index_name not in pinecone.list_indexes():
+        pinecone.create_index(name=index_name, dimension=1536, metric="cosine")
+    
+    index = pinecone.Index(index_name)
+
+    #embed the documents and save to pinecone
+    embeddings = OpenAIEmbeddings(openai_api_key)
+
+    db = Pinecone.from_documents(
+        chunks,
+        embeddings,
+        index_name=index_name
+    )
+
+    print(f"Saved {len(chunks)} chunks to Pinecone index {index_name}.")
 
 if __name__ == "__main__":
     #makes sure main() only runs when this script is called directly
